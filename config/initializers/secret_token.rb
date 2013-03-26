cb342a60207efb537b906003a2c440bd1a10eb5f
lab-cord <lab-cord@ama.(none)>
2013-03-26 10:07:25 -0300

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hospital::Application.config.secret_token = '5e66a734bf576089385a340fa3e8f4d918e073745fb47bcb63b848b2ffb3bdf5bac1e465717586709989592931bfc6a953feb0bd5ff9fb44ffe19005371468fe'
